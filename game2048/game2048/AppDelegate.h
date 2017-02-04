685c0cd7efb30d2655dd7515e4f5972f3f050d66
Abhineet Sharma <abhineetsharma@Abhineets-MacBook-Pro.local>
2017-02-04 18:53:28 -0500

    Initial commit

---
+++ game2048/game2048/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  game2048
+//
+//  Created by Abhineet Sharma on 2/4/17.
+//  Copyright © 2017 Abhineet Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ game2048/game2048/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  game2048
+//
+//  Created by Abhineet Sharma on 2/4/17.
+//  Copyright © 2017 Abhineet Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
